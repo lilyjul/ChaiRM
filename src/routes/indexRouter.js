504d0ae4f4d5bd2c78238e37cfc38d3b669c7391
Arman <dr.ihi.jekyll@gmail.com>
2021-10-08 17:58:38 +0300

    Merged 'ChaiRM' beta-version

--- src/routes/indexRouter.js
+++ src/routes/indexRouter.js
@@ -4,7 +4,6 @@ const { Customer, Order } = require('../db/models')
 router.get('/', async (req, res, next) => {
     const customersList = await Customer.findAll({ raw: true })
     const orderList = await Order.findAll({ raw: true })
-    console.log(orderList);
     res.render('index', { customersList, orderList })
 })
 
